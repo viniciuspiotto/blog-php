8f25ed359b08fbb42939b8cafb4f5112ac83b5b3
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2024-08-07 15:37:42 -0300

    feat(data): adding mocks to blog

---
+++ data/categories.php
@@ -0,0 +1,9 @@
+<?php
+
+  $categories = [
+    'PHP',
+    'Clean Architecture',
+    'Java',
+    'Computer Science',
+    'Database'
+  ];
---
+++ data/posts.php
@@ -0,0 +1,11 @@
+<?php
+
+  $posts = [
+    [
+      'id' => 1,
+      'title' => "Cleaning the code",
+      'description' => "What Martin Fowler's book Clean Architecture wants to tell us",
+      'tags' => ["Clean Architecture", "Java", "Computer Science"],
+      'img' => "clean_architecture.svg"
+    ],
+  ];
